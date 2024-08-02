df1bda819b16bcbc5d91b7220273d7525ffc941c
velosobr <linoc.veloso@gmail.com>
2024-08-02 13:48:50 -0300

    feat: Setting up the ReaderScreen Enum & App Routing

--- app/src/main/java/com/velosobr/bookbuddy/navigation/BookBuddyScreens.kt
+++ app/src/main/java/com/velosobr/bookbuddy/navigation/BookBuddyScreens.kt
@@ -1,4 +1,29 @@
 package com.velosobr.bookbuddy.navigation
 
 enum class BookBuddyScreens {
+
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    ReaderHomeScreen,
+    SearchScreen,
+    DetailScreen,
+    UpdateScreen,
+    ReaderStatsScreen;
+
+    companion object {
+        fun fromRoute(route: String): BookBuddyScreens
+        = when (route?.substringBefore("/")) {
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            ReaderHomeScreen.name -> ReaderHomeScreen
+            SearchScreen.name -> SearchScreen
+            DetailScreen.name -> DetailScreen
+            UpdateScreen.name -> UpdateScreen
+            ReaderStatsScreen.name -> ReaderStatsScreen
+            null -> ReaderHomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized.")
+        }
+    }
 }
